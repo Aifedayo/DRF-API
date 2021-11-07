ccd004c5f4090c6be6f7197ac0c54a6513941251
Aifedayo <akeemifedayolag@gmail.com>
2021-11-07 17:52:14 +0100

    making update to models

--- restapi/src/updates/models.py
+++ restapi/src/updates/models.py
@@ -7,5 +7,10 @@ def upload_update_image(instance, filename):
 
 class Update(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    content = models.TextField()
-    image = models.ImageField(upload_to="")
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=upload_update_image, blank=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content or ""
